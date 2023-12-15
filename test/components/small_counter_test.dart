e93890ae1b9aa515b822a21ae469ef5cb8ad22f1
Ales Zima <ales@cpex.cz>
2023-12-15 10:07:56 +0100

    Added SmallCounter unit test

---
+++ test/components/small_counter_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:my_2048/components/small_counter.dart';
+
+void main() {
+  group('SmallCounter', () {
+    testWidgets('displays the provided title and value', (widgetTester) async {
+      const title = 'best score';
+      const value = '32';
+
+      await widgetTester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: SmallCounter(
+              title: title,
+              value: value,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.text(title.toUpperCase()), findsOneWidget);
+      expect(find.text(value), findsOneWidget);
+    });
+  });
+}
