92ae92335024fc7a5f8c04e3a96d061e406aaeff
Ales Zima <ales@cpex.cz>
2023-12-18 15:27:37 +0100

    Added error handling for board invalid length

--- lib/components/board.dart
+++ lib/components/board.dart
@@ -10,16 +10,36 @@ class Board extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final gameTiles = ref.watch(boardProvider);
+    final hasError = gameTiles.length != 16;
+
+    final errorWidget = Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Text(
+          'Something went wrong, please restart the game!',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                fontSize: 16,
+                fontStyle: FontStyle.italic,
+              ),
+        ),
+      ),
+    );
+
     return SizedBox(
+      height: hasError ? 400 : null,
       width: MediaQuery.of(context).size.width > 400 ? 400 : double.infinity,
       child: Card(
-        child: GridView.count(
-          shrinkWrap: true,
-          crossAxisCount: 4,
-          padding: const EdgeInsets.all(4),
-          physics: const NeverScrollableScrollPhysics(),
-          children: List.generate(16, (index) => BoardTile(gameTiles[index])),
-        ),
+        child: hasError
+            ? errorWidget
+            : GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 4,
+                padding: const EdgeInsets.all(4),
+                physics: const NeverScrollableScrollPhysics(),
+                children:
+                    List.generate(16, (index) => BoardTile(gameTiles[index])),
+              ),
       ),
     );
   }
